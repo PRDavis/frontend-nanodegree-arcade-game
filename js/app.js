dd3ad4efa46533150dedd5a791e1286dd3bb2a2f
Paul Davis <paulandcindy@gmail.com>
2015-03-29 18:01:11 -0400

    refactor - added randSpeed function

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ this.x=0;
 this.y=row((Math.floor(Math.random() * (3-0)))); pick random row;
 this.height=81;//enemy hieght
 this.width;//enemy width
-this.speed=(Math.floor(Math.random() * (200 - 30)+30));
+this.speed=randSpeed();
 this.sprite = 'images/enemy-bug.png';
 this.leftSide=x;
 this.rightSide=x+this.width;
@@ -44,12 +44,28 @@ function row(num)
 
 }
 
+
+function randSpeed(){
+  (Math.floor(Math.random() * (200 - 30)+30));
+  return;
+}
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x+=(this.speed*dt);
+if (this.x >= 505)
+  {
+    this.x=-102;
+    //this.x=Math.random(Math.random) *(151-1)-175;
+    this.y=row((Math.floor(Math.random() * (3 - 0))));
+    this.speed=(Math.floor(Math.random() * (200 - 30)+30));
+    this.didCollide(this,player);
+  }
+return;
 }
 
 // Draw the enemy on the screen, required method for game
